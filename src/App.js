c9f0a19c5dce5d3076a544e6f5446042eb47d4f2
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2023-07-17 09:33:19 +0200

    Tic-Tac-Toe React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import confetti from "canvas-confetti";
+
+import Square from "./components/Square";
+import { WinnerModal } from "./components/WinnerModal";
+
+const TURNS = {
+  X: "×",
+  O: "○",
+};
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
+  // cargar partida o empezar nueva
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
+  const [winner, setWinner] = useState(null); // null no ganador, false empate
+
+  const checkWinner = (boardToCheck) => {
+    for (let combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
+  };
+
+  useEffect(() => {
+
+  }, [turn, board])
+
+  const checkEndgame = (newBoard) => {
+    return newBoard.every((square) => square !== null);
+  };
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return; // si hay algo en la posición o hay un ganador, no actualiza
+
+    // actualizar tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    // cambiar turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    // guardar partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
+
+    //revisar ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndgame(newBoard)) {
+      setWinner(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="board">
+        <h1>Tic-Tac-Toe</h1>
+        <button onClick={resetGame}>Reset</button>
+        <section className="game">
+          {board.map((_square, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            );
+          })}
+        </section>
+        <section className="turn">
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        </section>
+        <WinnerModal resetGame={resetGame} winner={winner}></WinnerModal>
+      </main>
     </div>
   );
 }
